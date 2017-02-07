5704e137b4874f13cdc2239e643062dbd929d721
Omar Sanchez <osanchez002@student.butte.edu>
2017-02-07 23:11:15 +0000

    Lab 1.4 submission

--- Lab13/lab13.cpp
+++ Lab13/lab13.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 
 int main() {
-   int birthRate_seconds = 85780618/8;
-   int deathRate_seconds = 85780618/11;
+   int birthRate = 85780618/8;
+   int deathRate = 85780618/11; 
+   
    int currentPopulation = 324473000;
-   int yearly = birthRate_seconds - deathRate_seconds;
+   int yearly = birthRate - deathRate;
    int user_yearDifference  = user_yearchoice - 2017;
-   int projected population = yearly * user_yearDifference + currentPopulation;
+   int projected_population = yearly * user_yearDifference + currentPopulation;
      
-     cout << "Based on the rate of the growing pupulation, we can approximate the growing population in the US" endl;
-     cout << "Choose the year you wish to view";
+     cout << "Based on the rate of the growing pupulation, we can approximate the growing population in the US"; endl
+     cout << "Choose the year you wish to view"; endl
      cin >> user_yearchoice;
-     cout << "Since the current population is approximately 324,473,000" as of 2017.";
-     cout << "The population in " << user_yearDifference << "years, or in " << user_yearchoice; <<  "will be: " << projected population;
+     cout << "Since the current population is approximately 324,473,000 as of 2017."; endl
+     cout << "The population in "<< user_yearchoice; <<  "will be: " << projected_population; 
      
     
  
---
+++ lab14/lab14.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int change, quarters, dimes, nickels, pennies;
+    int total, dollars, cents;
+    
+    
+        cout << "Enter the total amount of money you wish to input";
+        cin  >> change;
+    
+    quarters = change / 25; // how many quarters are in the total amount
+    change = change % 25; //calculating change 
+    dimes = change/10; // how many dimes are in the total amount
+    change = change % 10; //calculating change
+    nickels = change / 5;// how many nickels are in the total amount
+    pennies = change % 5; //calculating whats left
+    
+    cout << "Total number of quarters is " << quarters << endl;
+    cout << "Total number of dimes is " << dimes << endl;
+    cout << "Total number of nickels is " << nickels << endl;
+    cout << "Total number of pennies is " << pennies << endl;
+    
+    cout << "input your total amount (notice a tax of 10.9 percent)";
+    cin >> total;  // inputting tax on initial amount
+    static_cast<double> total  = static_cast<double> total % 10.9;
+    
+    dollars = total / 100;  // how many dollarss are in the total amount
+    total = total % 100; //calculating change
+    cents = total;
+    
+    cout << "Total dollar count is " << dollars << endl;
+    cout << "Total number of cents left is " << cents << endl;
+    
+    
+    return 0;    
+ }
