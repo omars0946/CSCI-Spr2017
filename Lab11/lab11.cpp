08ef4f0672886c9e96da0eb7b82914fcc3ab262f
Omar Sanchez <osanchez002@student.butte.edu>
2017-02-21 03:58:39 +0000

    Lab1.1 submission

---
+++ Lab11/lab11.cpp
@@ -0,0 +1,8 @@
+The computer will randomly generate a number from 1 to 25
+The computer will prompt the user to pick a number
+The user will pick a number
+If the user input is too low, it will display "higher 
+If they are too high, it will display "lower" 
+for each incorrect guess, the computer will deduct 1 point from the initial total, which is 10
+Once guessed correctly, the computer will display the score (10 - total number of guesses)
+
