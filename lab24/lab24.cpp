d9b335197d86541dfd532c992d4a9291b4c9e720
Omar Sanchez <osanchez002@student.butte.edu>
2017-03-03 05:40:05 +0000

    Lab2.4 submission

--- lab24/lab24.cpp
+++ lab24/lab24.cpp
@@ -23,31 +23,34 @@ void Random() { //random number generator,
     
     cout << Value1 << DisplayNumber << Value2 << endl; // displays the output
 
-random_Ibs_kilo = DisplayNumber;
-random_kilo_Ibs = random_Ibs_kilo;
+random_Ibs_kilo = DisplayNumber; //sets random number equal to variable
+random_kilo_Ibs = random_Ibs_kilo; //kilo to pounds = pounds to kilo
 
 return;
 }
 
 void Ibs_to_Kilo() {
     
-    double Pounds = random_Ibs_kilo;
+    double Pounds = random_Ibs_kilo;        //conversion
     double PoundsConverted = 0;
-    double random_kilo_Ibs = PoundsConverted;
-    PoundsConverted = Pounds * (0.453592);
+    
+    PoundsConverted = Pounds * (0.453592); //1 ibs = .453592 kilo
 
     cout << "For " << Pounds << " Pounds. You would recieve " << PoundsConverted << " Kilograms." << endl; 
 
+random_kilo_Ibs = PoundsConverted;
 return;
 }
 
 void Kilo_to_Ibs(){
-    double Kilo = random_kilo_Ibs;
+    double Kilo = random_kilo_Ibs;      //1 kilo is 2.20462 Ibs
     double KiloConverted = 0;
     
     KiloConverted = Kilo * (2.20462);
 
     cout << "For " << Kilo << " Kilograms. You would recieve " << KiloConverted << " Ibs." << endl; 
+    
+
 
 return;
 }
@@ -55,7 +58,7 @@ return;
 
 
 int main() {
-    
+                    //state functions
    Random();
    Ibs_to_Kilo();
    Kilo_to_Ibs();
