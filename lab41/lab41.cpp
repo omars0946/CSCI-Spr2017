28e68f6d4e84f7aad9670dffea3302139336f608
Omar Sanchez <osanchez002@student.butte.edu>
2017-04-12 06:47:07 +0000

    lab41 submission

--- lab41/lab41.cpp
+++ lab41/lab41.cpp
@@ -4,40 +4,43 @@ using namespace std;
 
 class ArrayTools
 {
-private:
-
 public:
 
 ArrayTools();
-ArrayTools(int);
+ArrayTools(int);                    //declaring constructors
 void Print(int);
-void Find_min(int i, int b);
+void Find_min(int i, int b);            //setting functions in class
 void Find_max(int i, int b);
 void Find_sum(int, int);
 void Search(int, int, int );
 void Is_sorted();
 };
-
-ArrayTools::ArrayTools{
+ArrayTools::ArrayTools(){
+    int i;
+    i = 0;
+}
+ArrayTools::ArrayTools(int){
     int i = 0;
-    
 }
 void ArrayTools::Print(int SIZE){
-    for (int i = 0; i <  SIZE; i++) {
+    for (int i = 0; i <  SIZE; i++) {//printing values
         int myArray[i];
         
       cout << "Values: " << myArray[i]<< endl;
     }    
+    return;
 }
 void ArrayTools::Find_min (int i, int b){
-    int myArray[i];
-    double min =  myArray[0];
-    for (int i = 1; i < b; i++) {
-        if (myArray[i]  <min){
-            min = myArray[i];
+    int myArray[i];                         //declaring Array
+    double min =  myArray[0];       //setting array to variable
+    for (int i = 1; i < b; i++) {//between numbers, the function will execute it til it bevcomes true
+        if (myArray[i]  <min){ //catchrs whether statment is true
+            min = myArray[i];   //equals it to variable
         } 
+        cout << min << endl;    //prints variable
+        return;
 }
-}
+
 void ArrayTools::Find_max (int i, int b){
     int myArray[i];
     double max =  myArray[0];
@@ -47,6 +50,7 @@ void ArrayTools::Find_max (int i, int b){
         }
             cout << max << endl;
 }
+return;
 }
 void ArrayTools::Find_sum(int i, int SIZE){
     int myArray[i];
@@ -55,6 +59,7 @@ void ArrayTools::Find_sum(int i, int SIZE){
         sum+=myArray[i];
 }
 cout << sum << endl;
+return;
 }
 void ArrayTools::Search(int value, int i, int SIZE){
     int Searching = 0;
@@ -66,13 +71,21 @@ void ArrayTools::Search(int value, int i, int SIZE){
 else {
     cout << endl;
 }
+return;
 }
-void Is_sorted(int i){
+void Is_sorted(int i, int SIZE){
+    i = 2
     int myArray[i];
-    double max =  myArray[0];
-    for (int i = 1; myArray[1] >= myArray[2]; i++) {
+ y
+    if (myArray[1] >= myArray[i]) {
         cout << "No."<<endl;
-           
+        i++ ;
+        else if  (myArray[1] >= myArray[i]) {
+        cout << "No."<<endl;
+        i++ ; }
+         for (int i = 1; i < SIZE; i++) {
+             cout << "Yes."
+         }
             
 }
 }
@@ -85,7 +98,7 @@ int main(){
     }
     ArrayTools a(myArray);
     
-    a.Print();
+    a.Print(int);
     cout<<"Min: "<<a.Find_min(0, 4)<<endl;
     cout<<"Max: "<<a.Find_max(5, 10)<<endl;
     cout<<"Sum = "<<a.Find_sum()<<endl;
